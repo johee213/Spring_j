86979e0f7121d2e183bb5d90d56954683802c3ee
johee213 <johee0111@gmail.com>
2026-06-12 17:50:41 +0900

    Delete repository directory

--- repository/ChatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.masil.repository;
-
-import com.example.masil.entity.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
-}
--- repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.masil.repository;
-
-import com.example.masil.entity.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
